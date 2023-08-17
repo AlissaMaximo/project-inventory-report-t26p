2b60868caa26c516cadebeccdca5c7144e3c0ba8
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-08-17 17:12:20 -0300

    Req 7 feito

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,53 @@
-class SimpleReport:
-    pass
+from inventory_report.inventory import Inventory
+from inventory_report.reports.report import Report
+from datetime import date
+from typing import List, Dict
+
+
+class SimpleReport(Report):
+    def __init__(self) -> None:
+        self.inventories: List[Inventory] = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    def get_oldest_manufacturing_date(self) -> str:
+        manufacturing_dates = [
+            product.manufacturing_date
+            for inventory in self.inventories
+            for product in inventory.data
+        ]
+
+        return min(manufacturing_dates)
+
+    def get_nearest_expiration_date(self) -> str:
+        all_valid_expiration_dates = [
+            product.expiration_date
+            for inventory in self.inventories
+            for product in inventory.data
+            if product.expiration_date >= str(date.today())
+        ]
+
+        return min(all_valid_expiration_dates)
+
+    def get_largest_inventory_company(self) -> str:
+        duos: Dict[str, int] = {}
+
+        for inventory in self.inventories:
+            for product in inventory.data:
+                if duos.get(product.company_name):
+                    duos[product.company_name] += 1
+                else:
+                    duos[product.company_name] = 1
+
+        return max(duos, key=lambda product: duos[product])
+
+    def generate(self) -> str:
+        return (
+            "Oldest manufacturing date: "
+            + self.get_oldest_manufacturing_date()
+            + "Closest expiration date: "
+            + self.get_nearest_expiration_date()
+            + "Company with the largest inventory: "
+            + self.get_largest_inventory_company()
+        )
