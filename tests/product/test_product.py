6b3016a3c983379d689a62957fc5c6ac0a9f3776
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-08-17 10:58:05 -0300

    Req 1 completo

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        "1", "bottle", "wee", "2023/01/01", "2030/02/02", "B1", "drink"
+    )
+    assert product.id == "1"
+    assert product.product_name == "bottle"
+    assert product.company_name == "wee"
+    assert product.manufacturing_date == "2023/01/01"
+    assert product.expiration_date == "2030/02/02"
+    assert product.serial_number == "B1"
+    assert product.storage_instructions == "drink"
