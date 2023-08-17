ee493cdafb86e906c1f313d766f9f09402cb1d7b
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-08-17 13:48:40 -0300

    Req 5 completp

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+from typing import Optional, List
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        self.__data = data or []
+
+    def add_data(self, data: List[Product]) -> None:
+        self.__data.extend(data)
+
+    @property
+    def data(self) -> List[Product]:
+        return self.__data
